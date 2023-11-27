8f2aef3ce44ba3c6c7cb4e9ddc3ce10aaadf1f85
Nitin Jaswal <62323476+walkerthecreator@users.noreply.github.com>
2023-11-27 22:04:55 +0530

    Initial commit

---
+++ calories.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MAX=0
+CURRENT=0
+
+if [ $# -ne 1 ]; then
+  echo "Please provide the input file as an argument."
+  exit 1
+fi
+
+while read line; do
+  if [ -z "$line" ]; then
+    if [ $CURRENT -gt $MAX ]; then
+      MAX=$CURRENT
+    fi
+
+    CURRENT=0
+  else
+    CURRENT=$(($CURRENT + $line))
+  fi
+done < "$1"
+
+if [ $CURRENT -gt $MAX ]; then
+  MAX=$CURRENT
+fi
+
+echo "The elf carrying the most calories has $MAX calories."
